00f1ed549a5a9f32d188d78cff72dbddb8b1fbae
jabari arrington <arrinjab001@warren.k12.in.us>
2024-10-23 08:32:40 -0400

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+#TODO: Enter your age as an int.  Using the input function.
+
+if age < 5:
+    # TODO: print the following "You should play with toys."
+elif age >= 5 and age <= 12:
+    # TODO: print the following "You can play outdoor game."
+# TODO: handle the 13 to 17 condition
+# TODO: handle the 18 or older condition.
+
